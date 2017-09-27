fa4bb3705d0662dca1959c94bac38e45096828b8
Tidemann Norli <anorli@csumb.edu>
2017-09-27 02:21:54 +0000

    made tests and moved twitter api calls to another module

---
+++ test/simple.js
@@ -0,0 +1,12 @@
+var add = require("../simple-function").add;
+var expect = require("chai").expect;
+
+/*
+describe("Add function", function() {
+    it("should return the sum of 2 numbers", function(done) {
+        expect(add(2,3)).to.equal(5);   
+        done;
+    })
+})
+
+*/
